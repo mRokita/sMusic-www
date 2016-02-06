345b7bd0cc4532645047bf039ed39b29873b4b20
mRokita <mrokita@mrokita.pl>
2016-02-06 20:36:16 +0100

    Dodaję timeout

--- radio_utils.py
+++ radio_utils.py
@@ -15,6 +15,10 @@ def un_escape(msg):
 
 def send_for_result(dct):
     conn = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    timeout = 2
+    if dct["request"] in ["get_artists", "get_tracks", "get_albums"]:
+        timeout = 6
+    conn.settimeout(timeout)
     conn.connect(("127.0.0.1", config.listen_port))
     conn.read()
     conn.send(escape(json.dumps({"request": "ok", "type": "www", "key": "a"})))
