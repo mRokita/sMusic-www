38d5f966ab8b0f7010e663253e36ec794940e2b8
mRokita <mrokita@mrokita.pl>
2016-02-07 20:55:24 +0100

    Poprawiam port

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ sMusic-www - config
 """
 ssl_cert_file = "/home/mrokita/ssl/cert.pem"
 ssl_key_file = "/home/mrokita/ssl/privkey.pem"
-listen_port = 3485
+listen_port = 3484
 listen_host = "0.0.0.0"
 radio_key = "jasfdljLWAUFEDJSDnqld"
